cfb637f12f0dca6fb4cc4f07b0fcc04ececa2ef9
Christoph Baldenhofer <CBaldenhofer@luxoft.com>
2018-08-31 16:57:58 +0200

    log prefix support

--- LogHelper.h
+++ LogHelper.h
@@ -2,16 +2,30 @@
 #define LOG_H
 
 //#define NO_LOG_SERIAL
+//#define LOG_PREFIX '/'		// enable prefix
+#define LOG_SERIAL_DEV Serial
 
 #ifndef NO_LOG_SERIAL
-  #define LOG_INIT() Serial.begin(115200)
-  #define LOG_INIT2(baudrate) Serial.begin(baudrate)
-  #define LOG_WAIT() while(!Serial) {delay(10);}
-  #define LOG_PRINT(msg) Serial.print(msg)
-  #define LOG_PRINTLN(msg) Serial.println(msg)
+  #define LOG_INIT() LOG_SERIAL_DEV.begin(115200)
+  #define LOG_INIT2(baudrate) LOG_SERIAL_DEV.begin(baudrate)
+  #define LOG_WAIT() while(!LOG_SERIAL_DEV) {delay(10);}
+
+#ifdef LOG_PREFIX
+  #define LOG_PRINT(msg) LOG_SERIAL_DEV.print(LOG_PREFIX); LOG_SERIAL_DEV.print(msg)
+  #define LOG_PRINTLN(msg) LOG_SERIAL_DEV.print(LOG_PREFIX); LOG_SERIAL_DEV.println(msg)
+#else
+  #define LOG_PRINT(msg) LOG_SERIAL_DEV.print(msg)
+  #define LOG_PRINTLN(msg) LOG_SERIAL_DEV.println(msg)
+#endif
   
-  #define LOG_PRINTF(msg, format) Serial.println(msg, format)
-  #define LOG_PRINTLNF(msg, format) Serial.println(msg, format)
+#ifdef LOG_PREFIX
+  #define LOG_PRINTF(msg, format) LOG_SERIAL_DEV.print(LOG_PREFIX); LOG_SERIAL_DEV.println(msg, format)
+  #define LOG_PRINTLNF(msg, format) LOG_SERIAL_DEV.print(LOG_PREFIX); LOG_SERIAL_DEV.println(msg, format)
+#else
+  #define LOG_PRINTF(msg, format) LOG_SERIAL_DEV.println(msg, format)
+  #define LOG_PRINTLNF(msg, format) LOG_SERIAL_DEV.println(msg, format)
+#endif
+  
 #else
   #define LOG_INIT()
   #define LOG_PRINT(msg)
