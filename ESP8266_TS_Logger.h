dd6dbcf8f79991eabbbca430f9cb63b99a5e3b0f
B1 <cbstar@web.de>
2019-05-30 14:35:26 +0200

    fix for latest esp

--- ESP8266_TS_Logger.h
+++ ESP8266_TS_Logger.h
@@ -1,6 +1,12 @@
 #ifndef ESP8266TSLOGGER_H
 #define ESP8266TSLOGGER_H
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
 #include <SoftwareSerial.h>
 
 class ESP8266TsLogger {
